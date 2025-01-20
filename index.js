23d5b010a9fb8f24438ca63004bf154bdb1dbbce
Edu <eduardonascimento60650@gmail.com>
2025-01-20 16:38:02 -0300

    Commitando mais

---
+++ index.js
@@ -0,0 +1,13 @@
+let tema = document.getElementById('tema')
+const temaCorpo = document.getElementById('corpo')
+let temaAtvio = true
+
+if (temaAtivo == true){
+
+    tema.addEventListener('click', ()=> {
+    
+
+    })    
+    
+}
+
