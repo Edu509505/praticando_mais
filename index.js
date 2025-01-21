b3119cb4498e9df9f46f58db2966f4bd4f365e98
Edu <eduardonascimento60650@gmail.com>
2025-01-20 22:30:28 -0300

    Fazendo alterações pendentes

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 let tema = document.getElementById('tema')
 const temaCorpo = document.getElementById('corpo')
-let temaAtvio = true
+let temaAtivo = true
 
-if (temaAtivo == true){
+tema.addEventListener('click', () => {
 
-    tema.addEventListener('click', ()=> {
-    
-
-    })    
-    
-}
-
+    document.body.classList.toggle('escuro')
+    document.getElementById('tema').classList.toggle('escuro')
+})
