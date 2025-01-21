b9448bd9b8bdc561cec5ccbb0e487487f6b0bcab
Edu <eduardonascimento60650@gmail.com>
2025-01-20 22:33:40 -0300

    Deixando o necessário no JS

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 let tema = document.getElementById('tema')
-const temaCorpo = document.getElementById('corpo')
-let temaAtivo = true
 
 tema.addEventListener('click', () => {
 
     document.body.classList.toggle('escuro')
-    document.getElementById('tema').classList.toggle('escuro')
+    tema.classList.toggle('escuro')
 })
